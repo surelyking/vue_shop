e02c4cb0996b6f3e51ab24b8231c33e294af35ce
surly1996 <449349576@qq.com>
2020-03-30 16:53:22 +0800

    完成了登陆功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button type="info" @click="loginOut">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginOut() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 全局css
+import './assets/css/globle.css'
+// 导入字体样式图标
+import './assets/fonts/iconfont.css'
+// 导入axios
+import axios from 'axios'
+//配置请求和根路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http =axios
+
 
 Vue.config.productionTip = false
 
